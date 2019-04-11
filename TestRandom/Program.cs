917028f4a09d9a96a36a310b6045d708d030d36a
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-04-11 12:34:11 +0100

    Add project files.

---
+++ TestRandom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
