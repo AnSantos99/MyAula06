6cb00af71e14445e812e3c62eb6150dd6ed48219
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-04-11 12:59:58 +0100

    Declaration of the random object

--- TestRandom/Program.cs
+++ TestRandom/Program.cs
@@ -6,7 +6,14 @@ namespace TestRandom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare random object
+            Random rnd = new Random();
+
+            // Generate random number between 1-6 (Next is a Method)
+            int dado = rnd.Next(1, 6);
+
+            // Print out random number
+            Console.WriteLine(dado);
         }
     }
 }
