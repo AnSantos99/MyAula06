4a0c392704155c0eb56ae2ad7c80fefaf86aabfa
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-04-11 13:11:46 +0100

    Ask User & add with random number

--- TestRandom/Program.cs
+++ TestRandom/Program.cs
@@ -9,11 +9,23 @@ namespace TestRandom
             // Declare random object
             Random rnd = new Random();
 
+            // Ask for user input
+            int userInp;
+            Console.WriteLine("Type a number of your choice:");
+            userInp = Convert.ToInt32(Console.ReadLine());
+
             // Generate random number between 1-6 (Next is a Method)
             int dado = rnd.Next(1, 6);
 
-            // Print out random number
-            Console.WriteLine(dado);
+            // Addition of user input with random dice number
+            int sum = userInp + dado;
+
+            // Print out results
+            Console.WriteLine($"User number was: {userInp}");
+            Console.WriteLine($"Random number was: {dado}");
+            Console.WriteLine($"The sum of both is: {sum}");
+
+            Console.ReadKey();
         }
     }
 }
